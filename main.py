d51127e61bd00ff622f71b0000de01e76df7545a
hywang <hywang@netdb.csie.ncku.edu.tw>
2023-12-28 06:48:51 +0800

    finish all

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt 
 from torchvision import transforms
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QMessageBox
 from PyQt6.QtWidgets import QApplication, QFileDialog, QMainWindow
 from interface import Ui_MainWindow
 from PyQt6 import QtWidgets
 from loguru import logger
+from PyQt6.QtGui import QPixmap
 from GraffitiBoard import GraffitiBoard
 from MNIST.model import VGG19BNforMNIST
 from backgroundSubtraction import background_subtraction
 from opticalFlow import preprocessing, video_tracking
 from dimensionReduction import dimension_reduction
 from MNIST.vgg19_bn_mnist_demo import show_mnist_structure, show_loss_and_accuracy
+from ResNet50.ResNet50_demo import show_images, show_model_structure, show_comparison, show_inference_catdog
 
 
 class MainApp(QMainWindow, Ui_MainWindow):
@@ -43,7 +48,11 @@ class MainApp(QMainWindow, Ui_MainWindow):
         self.pushButton_12.clicked.connect(self.run_inference_result)
         self.pushButton_10.clicked.connect(self.run_show_mnist_structure)
         self.pushButton_11.clicked.connect(self.run_show_loss_and_accuracy)
-
+        self.pushButton_15.clicked.connect(self.run_show_images)
+        self.pushButton_16.clicked.connect(self.run_show_model_structure)
+        self.pushButton_14.clicked.connect(self.load_picture)
+        self.pushButton_18.clicked.connect(self.run_show_inference_catdog)
+        self.pushButton_17.clicked.connect(self.run_show_comparison)
         # intialize 
         self.image_path = ""
         self.video_path = ""
@@ -51,6 +60,10 @@ class MainApp(QMainWindow, Ui_MainWindow):
         self.n_components = None
         self.model_path =  './model/vgg19_bn_mnist.pth'
         self.mnist_image_path = './MNIST/loss_and_accuracy.jpg'
+        self.inference_loader = None
+        self.ResNet_model_with = './model/cat_dog_with_RandoErasing.pth'
+        self.ResNet_model_without = './model/cat_dog_with_RandoErasing.pth'
+        self.current_image_path = ""
 
     def load_image(self):
         # Open a QFileDialog to select an image
@@ -152,8 +165,49 @@ class MainApp(QMainWindow, Ui_MainWindow):
         plt.xlabel("Class")
         plt.ylabel("Probability")
         plt.show()
+    
+    def load_images(self):
+        transform = transforms.Compose([
+                    transforms.Resize((224, 224)),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                ])
+        # Load the dataset
+        inference_dataset = datasets.ImageFolder('./dataset/inference_dataset', transform=transform)
+        self.inference_loader = DataLoader(inference_dataset, batch_size=32, shuffle=False)
+    
 
-            
+    def load_picture(self):
+        fname, _ = QFileDialog.getOpenFileName(self, 'Load Image', '', 'Image files (*.jpg *.jpeg *.png *.bmp *.gif)')
+        if fname:
+            pixmap = QPixmap(fname)
+            scaled_pixmap = pixmap.scaled(128, 128, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
+            self.current_image_path = fname
+
+            # Save the scaled pixmap for later use in inference
+            self.current_pixmap = scaled_pixmap
+
+            scene = QtWidgets.QGraphicsScene(self)
+            scene.addPixmap(scaled_pixmap)
+            self.graphicsView_2.setScene(scene)
+            self.graphicsView_2.fitInView(scene.itemsBoundingRect(), Qt.AspectRatioMode.KeepAspectRatio)
+            self.graphicsView_2.show()
+
+    def run_show_images(self):
+        show_images()
+    
+    def run_show_model_structure(self):
+        show_model_structure(self.ResNet_model_with)
+    
+    def run_show_comparison(self):
+        show_comparison()
+
+    def onTextChange(self, text):
+        self.word = text
+
+    def run_show_inference_catdog(self):
+        prediction = show_inference_catdog(self.ResNet_model_with, self.current_image_path)
+        self.label.setText(f'Prediction: {prediction}')
         
 
 if __name__ == "__main__":
